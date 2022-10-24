b0f0ea56f892c5888b53a5559dc11bb8c08cea2d
serheiev <69833095+serheiev@users.noreply.github.com>
2022-10-24 17:29:23 +0300

    fix index

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import { Header } from './Header/Header';
-// import { HomePage } from '../Pages/HomePage/HomePage';
-// import { SearchPage } from 'Pages/SearchPage/SearchPage';
-// import { MovieDetails } from './MovieDetails/MovieDetails';
-// import { MovieCast } from './MovieCast/MovieCast';
-// import { MovieReviews } from './MovieReviews/MovieReviews';
 import { Loader } from './Loader/Loader';
 
 const LazyHomePage = lazy(() => import('../Pages/HomePage/HomePage'));
@@ -19,10 +14,8 @@ export const App = () => {
     <>
       <Header />
       <Suspense fallback={<Loader />}>
-        {/* <Route path="/" element={<LazyHeader />}></Route> */}
-
         <Routes>
-          <Route index element={<LazyHomePage />} />
+          <Route path="/" element={<LazyHomePage />} />
           <Route path="/movies" element={<LazySearchPage />} />
           <Route path="/movies/:movieId" element={<LazyMovieDetails />}>
             <Route path="cast" element={<LazyMovieCast />} />
